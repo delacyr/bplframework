f7b67fddcc307e2ca021234d51d45497f5f667a2
Delacyr <delacyr@gmail.com>
2018-08-24 19:01:53 -0400

    leaving only must-have packages

--- bpmn2/org.eclipse.bpmn2.tests/src/org/eclipse/bpmn2/tests/isolated/LoadXMIPluginTest.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.bpmn2.tests.isolated;
-
-public class LoadXMIPluginTest extends LoadXMLPluginTest {
-    protected String getFilename() {
-        return "res/basic.bpmn2xmi";
-    };
-}
